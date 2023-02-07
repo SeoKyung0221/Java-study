bc27489cd5cece5be8a8edd1130c51c643ca7aae
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-02-08 04:34:11 +0900

    Delete OperatorEx20.java

--- OperatorEx20.java
+++
@@ -1,7 +0,0 @@
-public class OperatorEx20 {
-	public static void main(String[] args) {
-		System.out.println(-10%8); 
-		System.out.println(10%-8);
-		System.out.println(-10%-8);
-	} // 나머지 연산자는 나누는 수로 음수도 허용하지만 부호는 무시! 즉, 절대값으로 나눈다.
-}
