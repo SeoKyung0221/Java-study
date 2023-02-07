6b5dd58c306ffba88a9d132ba0640045ca725b7c
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-02-08 04:33:50 +0900

    Delete OperatorEx18.java

--- OperatorEx18.java
+++
@@ -1,8 +0,0 @@
-public class OperatorEx18 {
-	public static void main(String[] args) {
-		double pi=3.141592;
-		double shortPi=Math.round(pi*1000)/1000.0;
-		System.out.println(shortPi);
-	} // 예제 17번에서 Math.round를 이용하여 반올림하는 방법 round 메서드는 소수 첫째자리에서 반올림하여
-	  // 정수를 반환하는 함수이다. 
-}
