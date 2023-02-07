c7281910eb7827f527d1d0372c247e99a4c35f38
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-02-08 04:35:47 +0900

    Delete OperatorEx6.java

--- OperatorEx6.java
+++
@@ -1,8 +0,0 @@
-public class OperatorEx6 {
-	public static void main(String[] args) {
-		byte a=10;
-		byte b=30;
-		byte c = (byte)(a*b); // a, b가 연산되면서 int형으로 범위가 바뀌어 byte타입인 c로는 형변환으로 변환 후 저장.
-		System.out.println(c);
-	}
-}
