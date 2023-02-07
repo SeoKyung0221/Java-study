17b37980091474741b122588a08194d531749eb1
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-02-08 04:33:39 +0900

    Delete OperatorEx16.java

--- OperatorEx16.java
+++
@@ -1,7 +0,0 @@
-public class OperatorEx16 {
-	public static void main(String[] args) {
-		float pi=3.141592f;
-		float shortPi=(int)(pi*1000) / 1000f; // pi*1000의 소수점 이하 숫자는 (int)형변환으로 버려진다. 
-		System.out.println(shortPi);
-	}
-}
