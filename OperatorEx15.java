5b9e2ed869839251c5dc888deb042f4f7cc59a47
SeoKyung0221 <121633298+SeoKyung0221@users.noreply.github.com>
2023-02-08 04:33:34 +0900

    Delete OperatorEx15.java

--- OperatorEx15.java
+++
@@ -1,7 +0,0 @@
-public class OperatorEx15 {
-	public static void main(String[] args) {
-		char lowerCase='a';
-		char upperCase=(char)(lowerCase-32); // a의 유니코드 97에서 32를 빼면 65로 A의 유니코드다.
-		System.out.println(upperCase);
-	}   
-}
